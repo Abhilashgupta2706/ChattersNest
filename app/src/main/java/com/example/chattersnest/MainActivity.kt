3c045822b06d0ac374df3bff496dad23347e8ac0
Abhilash Gupta <88545049+Abhilashgupta2706@users.noreply.github.com>
2022-08-09 12:32:14 +0530

    Main Activity - Users List

--- app/src/main/java/com/example/chattersnest/MainActivity.kt
+++ app/src/main/java/com/example/chattersnest/MainActivity.kt
@@ -5,23 +5,50 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var rvUser: RecyclerView
     private lateinit var userList: ArrayList<User>
     private lateinit var adapter: UserAdapter
     private lateinit var mAuth: FirebaseAuth
+    private lateinit var mdbRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         mAuth = FirebaseAuth.getInstance()
+        mdbRef = FirebaseDatabase.getInstance().getReference()
 
         userList = ArrayList()
         adapter = UserAdapter(this, userList)
+
+        rvUser = findViewById(R.id.rvUsers)
+        rvUser.layoutManager = LinearLayoutManager(this)
+        rvUser.adapter = adapter
+
+        mdbRef.child("user").addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                userList.clear()
+                for (postSnapShot in snapshot.children) {
+                    val currentUser = postSnapShot.getValue(User::class.java)
+
+                    if (mAuth.currentUser?.uid != currentUser?.uid) {
+                        userList.add(currentUser!!)
+                    }
+                }
+
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
